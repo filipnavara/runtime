16f35d0ade4a38b8288581b99a26bba8ed1f3b7f
Filip Navara <filip.navara@gmail.com>
2025-03-02 18:17:46 +0100

    Simplify test case

--- src/mono/tests/GCBridge/main.c
+++ src/mono/tests/GCBridge/main.c
@@ -103,34 +103,28 @@ void test2(SgenBridgeProcessor *bridge_processor)
 {
     bridge_processor->reset_data();
 
-    MonoObject *o_runnableImplementor = alloc_object(&klass_RunnableImplementor, 1);
-    MonoObject *o_runnableImplementor2 = alloc_object(&klass_RunnableImplementor, 1);
-    MonoObject *o_runnableImplementor3 = alloc_object(&klass_RunnableImplementor, 1);
-    MonoObject *o_byteArrayOutputStream = alloc_object(&klass_ByteArrayOutputStream, 0);
+    MonoObject *o_fanin1 = alloc_object(&klass_Bridagable, 1);
+    MonoObject *o_fanin2 = alloc_object(&klass_Bridagable, 1);
+    MonoObject *o_fanin3 = alloc_object(&klass_Bridagable, 1);
+    MonoObject *o_fanout = alloc_object(&klass_Bridagable, 0);
 
-    MonoObject *o_action = alloc_object(&klass_Action, 100 * 2);
+    MonoObject *o_action = alloc_object(&klass_NonBridagable, 100);
     for (int i = 0; i < 100; i++)
     {
-        MonoObject *o_displayClass = alloc_object(&klass_DisplayClass, 1);
-        MonoObject *o_asyncStateMachineBox = alloc_object(&klass_AsyncStateMachineBox, 2);
-        MonoObject *o_asyncStreamWriter = alloc_object(&klass_AsyncStreamWriter, 2);
-        o_displayClass->refs[0] = o_action;
-        o_asyncStateMachineBox->refs[0] = o_asyncStreamWriter;
+        MonoObject *o_asyncStateMachineBox = alloc_object(&klass_NonBridagable, 2);
+        o_asyncStateMachineBox->refs[0] = o_fanout;
         o_asyncStateMachineBox->refs[1] = o_action;
-        o_asyncStreamWriter->refs[0] = o_byteArrayOutputStream;
-        o_asyncStreamWriter->refs[1] = o_asyncStateMachineBox;
-        o_action->refs[i * 2] = o_displayClass;
-        o_action->refs[i * 2 + 1] = o_asyncStateMachineBox;
+        o_action->refs[i] = o_asyncStateMachineBox;
     }
 
-    o_runnableImplementor->refs[0] = o_action;
-    o_runnableImplementor2->refs[0] = o_action;
-    o_runnableImplementor3->refs[0] = o_action;
+    o_fanin1->refs[0] = o_action;
+    o_fanin2->refs[0] = o_action;
+    o_fanin3->refs[0] = o_action;
 
-    bridge_processor->register_finalized_object(o_byteArrayOutputStream);
-    bridge_processor->register_finalized_object(o_runnableImplementor);
-    bridge_processor->register_finalized_object(o_runnableImplementor2);
-    bridge_processor->register_finalized_object(o_runnableImplementor3);
+    bridge_processor->register_finalized_object(o_fanout);
+    bridge_processor->register_finalized_object(o_fanin1);
+    bridge_processor->register_finalized_object(o_fanin2);
+    bridge_processor->register_finalized_object(o_fanin3);
 
     bridge_processor->processing_stw_step();
     bridge_processor->processing_build_callback_data(0);

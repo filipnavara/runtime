7accdd4144bfc5a6f9342784257e70ffe4faabd3
Filip Navara <filip.navara@gmail.com>
2026-05-17 22:34:27 +0200

    Add PPC64LE unsigned float to long casts

--- src/coreclr/jit/emitppc64le.cpp
+++ src/coreclr/jit/emitppc64le.cpp
@@ -570,6 +570,7 @@ size_t emitter::emitOutputInstr(insGroup* ig, instrDesc* id, BYTE** dp)
         case INS_frsp:
         case INS_fctiwz:
         case INS_fctidz:
+        case INS_fctiduz:
         case INS_fcfid:
         case INS_fcfids:
             code = code | (ppcFReg(id->idReg1()) << 21) | (ppcFReg(id->idReg2()) << 11);

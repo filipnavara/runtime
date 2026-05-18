5975a7af32dccd05d73f70ae72cebefa9e45c549
Filip Navara <filip.navara@gmail.com>
2026-05-18 22:51:37 +0200

    Preserve PPC64LE TOC relocation addends

--- src/coreclr/tools/Common/Compiler/ObjectWriter/ObjectWriter.cs
+++ src/coreclr/tools/Common/Compiler/ObjectWriter/ObjectWriter.cs
@@ -578,7 +578,7 @@ namespace ILCompiler.ObjectWriter
                         blockToRelocate.Data.AsSpan(reloc.Offset),
                         reloc.RelocType,
                         relocSymbolName,
-                        relocTarget.Offset);
+                        relocTarget.Offset + reloc.Addend);
 
                     if (_options.HasFlag(ObjectWritingOptions.ControlFlowGuard))
                     {

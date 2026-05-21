44f867b88919974f585771a94baff11ba40090b0
Filip Navara <filip.navara@gmail.com>
2026-05-21 21:53:42 +0200

    Fix PPC64LE small call GC register masks

--- src/coreclr/jit/emitinl.h
+++ src/coreclr/jit/emitinl.h
@@ -414,8 +414,9 @@ inline ssize_t emitter::emitGetInsAmdAny(const instrDesc* id) const
     id->idReg2((regNumber)encodeMask); // Save in idReg2
 
 #elif defined(TARGET_POWERPC64)
-    assert(REGNUM_BITS >= 9);
-    assert((regmask & ~RBM_INT_CALLEE_SAVED) == RBM_NONE);
+    assert(REGNUM_BITS >= 7);
+    assert((regmask & ~(RBM_R14 | RBM_R15 | RBM_R16 | RBM_R17 | RBM_R18 | RBM_R19 | RBM_R20 | RBM_R21 | RBM_R22 |
+                        RBM_R23 | RBM_R24 | RBM_R25 | RBM_R26 | RBM_R27)) == RBM_NONE);
 
     encodeMask = 0;
 
@@ -433,31 +434,25 @@ inline ssize_t emitter::emitGetInsAmdAny(const instrDesc* id) const
         encodeMask |= 0x020;
     if ((regmask & RBM_R20) != RBM_NONE)
         encodeMask |= 0x040;
-    if ((regmask & RBM_R21) != RBM_NONE)
-        encodeMask |= 0x080;
-    if ((regmask & RBM_R22) != RBM_NONE)
-        encodeMask |= 0x100;
 
     id->idReg1((regNumber)encodeMask); // Save in idReg1
 
     encodeMask = 0;
 
-    if ((regmask & RBM_R23) != RBM_NONE)
+    if ((regmask & RBM_R21) != RBM_NONE)
         encodeMask |= 0x001;
-    if ((regmask & RBM_R24) != RBM_NONE)
+    if ((regmask & RBM_R22) != RBM_NONE)
         encodeMask |= 0x002;
-    if ((regmask & RBM_R25) != RBM_NONE)
+    if ((regmask & RBM_R23) != RBM_NONE)
         encodeMask |= 0x004;
-    if ((regmask & RBM_R26) != RBM_NONE)
+    if ((regmask & RBM_R24) != RBM_NONE)
         encodeMask |= 0x008;
-    if ((regmask & RBM_R27) != RBM_NONE)
+    if ((regmask & RBM_R25) != RBM_NONE)
         encodeMask |= 0x010;
-    if ((regmask & RBM_R28) != RBM_NONE)
+    if ((regmask & RBM_R26) != RBM_NONE)
         encodeMask |= 0x020;
-    if ((regmask & RBM_R29) != RBM_NONE)
+    if ((regmask & RBM_R27) != RBM_NONE)
         encodeMask |= 0x040;
-    if ((regmask & RBM_R30) != RBM_NONE)
-        encodeMask |= 0x080;
 
     id->idReg2((regNumber)encodeMask); // Save in idReg2
 
@@ -632,7 +627,7 @@ inline ssize_t emitter::emitGetInsAmdAny(const instrDesc* id) const
         regmask |= RBM_S11;
 
 #elif defined(TARGET_POWERPC64)
-    assert(REGNUM_BITS >= 9);
+    assert(REGNUM_BITS >= 7);
     encodeMask = id->idReg1();
 
     if ((encodeMask & 0x001) != 0)
@@ -649,29 +644,23 @@ inline ssize_t emitter::emitGetInsAmdAny(const instrDesc* id) const
         regmask |= RBM_R19;
     if ((encodeMask & 0x040) != 0)
         regmask |= RBM_R20;
-    if ((encodeMask & 0x080) != 0)
-        regmask |= RBM_R21;
-    if ((encodeMask & 0x100) != 0)
-        regmask |= RBM_R22;
 
     encodeMask = id->idReg2();
 
     if ((encodeMask & 0x001) != 0)
-        regmask |= RBM_R23;
+        regmask |= RBM_R21;
     if ((encodeMask & 0x002) != 0)
-        regmask |= RBM_R24;
+        regmask |= RBM_R22;
     if ((encodeMask & 0x004) != 0)
-        regmask |= RBM_R25;
+        regmask |= RBM_R23;
     if ((encodeMask & 0x008) != 0)
-        regmask |= RBM_R26;
+        regmask |= RBM_R24;
     if ((encodeMask & 0x010) != 0)
-        regmask |= RBM_R27;
+        regmask |= RBM_R25;
     if ((encodeMask & 0x020) != 0)
-        regmask |= RBM_R28;
+        regmask |= RBM_R26;
     if ((encodeMask & 0x040) != 0)
-        regmask |= RBM_R29;
-    if ((encodeMask & 0x080) != 0)
-        regmask |= RBM_R30;
+        regmask |= RBM_R27;
 
 #else
     NYI("unknown target");

4f8a310ec7c385910def75d164c92f13701bcde2
Filip Navara <filip.navara@gmail.com>
2026-05-21 13:57:56 +0200

    Route PPC64LE runtime imports through local wrappers

--- src/coreclr/tools/aot/ILCompiler.RyuJit/Compiler/DependencyAnalysis/RyuJitNodeFactory.cs
+++ src/coreclr/tools/aot/ILCompiler.RyuJit/Compiler/DependencyAnalysis/RyuJitNodeFactory.cs
@@ -32,11 +32,6 @@ namespace ILCompiler.DependencyAnalysis
                 }
                 else if (method.HasCustomAttribute("System.Runtime", "RuntimeImportAttribute"))
                 {
-                    if (Target.Architecture == TargetArchitecture.Ppc64le && !Target.IsWindows && Ppc64leRuntimeImportMethodNode.ShouldUseThunk(method))
-                    {
-                        return new Ppc64leRuntimeImportMethodNode(method, NameMangler);
-                    }
-
                     return new RuntimeImportMethodNode(method, NameMangler);
                 }
             }

c8b5a62771ecd6c406dec8391f95d53c0feb6446
Filip Navara <filip.navara@gmail.com>
2026-05-22 18:21:14 +0200

    Enable PPC64 FP struct lowering in CoreCLR

--- src/coreclr/vm/jitinterface.cpp
+++ src/coreclr/vm/jitinterface.cpp
@@ -9795,7 +9795,7 @@ void CEEInfo::getFpStructLowering(CORINFO_CLASS_HANDLE structHnd, CORINFO_FPSTRU
 
     JIT_TO_EE_TRANSITION();
 
-#if defined(TARGET_RISCV64) || defined(TARGET_LOONGARCH64)
+#if defined(TARGET_RISCV64) || defined(TARGET_LOONGARCH64) || defined(TARGET_POWERPC64)
     FpStructInRegistersInfo info = MethodTable::GetFpStructInRegistersInfo(TypeHandle(structHnd));
     if (info.flags != FpStruct::UseIntCallConv)
     {
@@ -9827,7 +9827,7 @@ void CEEInfo::getFpStructLowering(CORINFO_CLASS_HANDLE structHnd, CORINFO_FPSTRU
     {
         pLowering->byIntegerCallConv = true;
     }
-#endif // TARGET_RISCV64 || TARGET_LOONGARCH64
+#endif // TARGET_RISCV64 || TARGET_LOONGARCH64 || TARGET_POWERPC64
 
     EE_TO_JIT_TRANSITION();
 }
